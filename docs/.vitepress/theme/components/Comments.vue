2c36c23ffc028860826cd834423b8338def3058f
helloQianFan <3800047396@qq.com>
2023-03-15 20:07:15 +0800

    gitalk 修改配置

--- docs/.vitepress/theme/components/Comments.vue
+++ docs/.vitepress/theme/components/Comments.vue
@@ -13,14 +13,16 @@ const gitalk = new Gitalk({
   clientID: "53f859bc548f68175265",
   clientSecret: "73f528b5f57c8ca681785d711ee9904f197671ec",
   repo: "blog-comments",
-  owner: "helloahao",
-  admin: ["helloahao096"],
+  owner: "helloAhao096", //github仓库用户名
+  admin: ["helloAhao096"], // 可以初始化插件的用户
   id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
   language: "zh-CN",
   distractionFreeMode: true, // Facebook-like distraction free mode
 });
 
+
 onMounted(() => {
+  console.log(location.pathname.substring(0, 50))
   gitalk.render("gitalk-container");
 });
 </script>
